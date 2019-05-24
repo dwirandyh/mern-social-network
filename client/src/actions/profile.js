f7e2d85250bc5031ca8be10bc427bc73e57192b2
Dwi Randy H <dwirandyherdinanto@gmail.com>
2019-05-24 17:44:26 +0700

    profile page
    - top
    - education
    -experience
    - githubrepos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -65,7 +65,7 @@ export const getProfileById = userId => async dispatch => {
 // Get github repo
 export const getGithubRepos = username => async dispatch => {
   try {
-    const res = await axios.get("/api/profile/github");
+    const res = await axios.get("/api/profile/github/" + username);
 
     dispatch({
       type: GET_REPOS,
